f87ab27c3c81f26373cc9b3245c0f51e5e5bf24f
406798106@qq.com <406798106@qq.com>
2017-02-17 09:36:20 +0800

    翼生活社区APP

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,85 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var useref = require('gulp-useref');
+var gulpIf = require('gulp-if');
+var del = require('del');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
 
 var paths = {
   sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['mergeMiniJsCss','minifyCss','minifyJs','minImages','copyHtml','copyLib']);
+
+/* 自动添加css兼容前缀任务*/
+gulp.task('autoprefixer', function () {
+  gulp.src(['./www/css/**/*.css','!./www/css/ionic.app.css','!./www/css/ionic.app.min.css'])
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'Android >= 4.0'],//last 2 versions: 主流浏览器的最新两个版本   Android for Android WebView.
+      cascade: true, //是否美化属性值 默认：true 像这样：
+      //-webkit-transform: rotate(45deg);
+      //        transform: rotate(45deg);
+      remove: false //是否去掉不必要的前缀 默认：true
+    }))
+    .pipe(gulp.dest('./www/css'));
+});
+
+/*  拼接合并压缩html中的js css减少http请求和大小
+ css link前后添加   <!-- build:css css/all.css -->     <!-- endbuild -->
+ js script  前后添加  <!-- build:js js/all.js -->  <!-- endbuild -->*/
+gulp.task('mergeMiniJsCss', function () {
+  return gulp.src('./www/index.html')
+    .pipe(useref())
+    // 当是css文件时候压缩
+    .pipe(gulpIf('*.css', minifyCss()))
+    // 当是Javascript文件时候压缩
+    .pipe(gulpIf('*.js', uglify({
+        mangle: false //不混淆变量名
+      }
+    )))
+    .pipe(gulp.dest('./yilife/'));
+});
+
+/*压缩css*/
+gulp.task('minifyCss', function () {
+  return gulp.src('./www/css/**/*.css')      //压缩的文件
+    .pipe(minifyCss())    //执行压缩
+    .pipe(gulp.dest('./yilife/css/'));  //输出文件夹
+});
+
+/*压缩js*/
+gulp.task('minifyJs', function () {
+  return gulp.src('./www/js/**/*.js/')
+    .pipe(uglify())    //压缩
+    .pipe(gulp.dest('./yilife/js/'));  //输出
+});
+
+/*压缩图片*/
+gulp.task('minImages', function(){
+  return gulp.src('./www/img/**/*.+(png|jpg|gif|svg)')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('./yilife/img/'));
+});
+
+/*复制html*/
+gulp.task('copyHtml', function () {
+  return gulp.src('./www/templates/**/*.html')      //复制的文件
+    .pipe(gulp.dest('./yilife/templates/'));  //输出文件夹
+});
+
+/*复制Lib*/
+gulp.task('copyLib', function () {
+  return gulp.src('./www/lib/**')      //复制的文件
+    .pipe(gulp.dest('./yilife/lib/'));  //输出文件夹
+});
+/*执行压缩前，先删除文件夹里的内容*/
+gulp.task('clean', function (cb) {
+  del(['./yilife/css', './yilife/js'], cb);
+});
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
@@ -26,7 +99,7 @@ gulp.task('sass', function(done) {
     .on('end', done);
 });
 
-gulp.task('watch', ['sass'], function() {
+gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
 });
 
---
+++ www/js/controllers.js
@@ -0,0 +1,15 @@
+angular.module('starter.controllers', [])
+  .config(function ($httpProvider) { //统一配置设置
+    //服务注册到$httpProvider.interceptors中  用于接口授权
+    $httpProvider.interceptors.push('MyInterceptor');
+    /* $httpProvider.defaults.headers.common['Authorization'] = localStorage.getItem('token');*/
+    /*    $http.defaults.cache = true/false;*/
+  })
+
+
+  //APP首页面
+  .controller('MainCtrl', function ($scope, $rootScope, CommonService, $ionicHistory) {
+
+  })
+
+
