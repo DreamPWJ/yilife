88676050f259e5619be1e35e90a75aae7a24f024
406798106@qq.com <a18863302302>
2017-03-20 17:53:58 +0800

    翼生活社区APP

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -34,73 +34,76 @@ angular.module('starter.controllers', [])
 
     }
 
-    cordova.ThemeableBrowser.open('http://118.123.168.19:8888/shouji/indexs', '_blank', {
-      statusbar: {
-        color: '#ffffffff'
-      },
-      toolbar: {
-        height: 44,
-        color: '#f0f0f0ff'
-      },
-      title: {
-        color: '#003264ff',
-        showPageTitle: true
-      },
-      backButton: {
-        image: 'back',
-        imagePressed: 'back_pressed',
-        align: 'left',
-        event: 'backPressed'
-      },
-      forwardButton: {
-        image: 'forward',
-        imagePressed: 'forward_pressed',
-        align: 'left',
-        event: 'forwardPressed'
-      },
-      closeButton: {
-        image: 'close',
-        imagePressed: 'close_pressed',
-        align: 'left',
-        event: 'closePressed'
-      },
-      customButtons: [
-        {
-          image: 'share',
-          imagePressed: 'share_pressed',
-          align: 'right',
-          event: 'sharePressed'
-        }
-      ],
-      menu: {
-        image: 'menu',
-        imagePressed: 'menu_pressed',
-        title: 'Test',
-        cancel: 'Cancel',
-        align: 'right',
-        items: [
+    $scope.openWeb=function() {
+      cordova.ThemeableBrowser.open('http://118.123.168.19:8888/shouji/indexs', '_blank', {
+        statusbar: {
+          color: '#ffffffff'
+        },
+        toolbar: {
+          height: 0,
+          color: '#f0f0f0ff'
+        },
+        title: {
+          color: '#003264ff',
+          showPageTitle: true
+        },
+        backButton: {
+          image: 'back',
+          imagePressed: 'back_pressed',
+          align: 'left',
+          event: 'backPressed'
+        },
+        forwardButton: {
+          image: 'forward',
+          imagePressed: 'forward_pressed',
+          align: 'left',
+          event: 'forwardPressed'
+        },
+        closeButton: {
+          image: 'close',
+          imagePressed: 'close_pressed',
+          align: 'left',
+          event: 'closePressed'
+        },
+        customButtons: [
           {
-            event: 'helloPressed',
-            label: 'Hello World!'
-          },
-          {
-            event: 'testPressed',
-            label: 'Test!'
+            image: 'share',
+            imagePressed: 'share_pressed',
+            align: 'right',
+            event: 'sharePressed'
           }
-        ]
-      },
-      backButtonCanClose: true
-    }).addEventListener('backPressed', function(e) {
-      alert('back pressed');
-    }).addEventListener('helloPressed', function(e) {
-      alert('hello pressed');
-    }).addEventListener('sharePressed', function(e) {
-      alert(e.url);
-    }).addEventListener(cordova.ThemeableBrowser.EVT_ERR, function(e) {
-      console.error(e.message);
-    }).addEventListener(cordova.ThemeableBrowser.EVT_WRN, function(e) {
-      console.log(e.message);
-    });
+        ],
+        menu: {
+          image: 'menu',
+          imagePressed: 'menu_pressed',
+          title: 'Test',
+          cancel: 'Cancel',
+          align: 'right',
+          items: [
+            {
+              event: 'helloPressed',
+              label: 'Hello World!'
+            },
+            {
+              event: 'testPressed',
+              label: 'Test!'
+            }
+          ]
+        },
+        backButtonCanClose: true
+      }).addEventListener('backPressed', function (e) {
+        alert('back pressed');
+      }).addEventListener('helloPressed', function (e) {
+        alert('hello pressed');
+      }).addEventListener('sharePressed', function (e) {
+        alert(e.url);
+      }).addEventListener(cordova.ThemeableBrowser.EVT_ERR, function (e) {
+        console.error(e.message);
+      }).addEventListener(cordova.ThemeableBrowser.EVT_WRN, function (e) {
+        console.log(e.message);
+      });
+    }
+
   })
 
 
